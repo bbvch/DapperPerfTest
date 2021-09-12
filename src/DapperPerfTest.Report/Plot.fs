0992b7a903878910e89aafb6794f69ceff8114d2
Raphael Schweizer <raphael.schweizer@bbv.ch>
2021-09-12 02:11:11 +0200

    switch to Plotly.Net

--- src/DapperPerfTest.Report/Plot.fs
+++ src/DapperPerfTest.Report/Plot.fs
@@ -1,10 +1,11 @@
 module DapperPerfTest.Report.Plot
 
-open CsvHelper
 open System.Globalization
 open System.IO
-open XPlot.Plotly
+open CsvHelper
 open DapperPerfTest.PerfTest
+open Plotly.NET
+open Plotly.NET.StyleParam
 
 type Benchmark() =
   member val Method = "" with get, set
@@ -38,20 +39,16 @@ let main _ =
             measurements
             |> List.filter (fun r -> r.Method = framework)
             |> List.map (fun r -> (r.Type, r.``Mean [ms]``))
-        Bar(
-            name = name,
-            x = (data |> Seq.map fst),
-            y = (data |> Seq.map snd))
+        Chart.Column(data |> Seq.map fst, data |> Seq.map snd, name)
 
     let groupedLayout =
-        let legend = Legend(xanchor = "center", x = 0.5, yanchor = "top", orientation = "h")
-        let yAxis = Yaxis(title = "Zeit [ms]")
-        let font = Font(family = "Times New Roman")
-        Layout(barmode = "group", legend = legend, yaxis = yAxis, font = font)
+        let legend = Legend.init(XAnchor = XAnchorPosition.Center, X = 0.5, YAnchor = YAnchorPosition.Top, Orientation = Orientation.Horizontal)
+        Layout.init(Barmode = Barmode.Group, Font = Font.init(Family = FontFamily.Times_New_Roman), Legend = legend)
 
     [ dataFor "Dapper" "Dapper"; dataFor "EfCore" "EF Core"; dataFor "EfCoreCompiled" "EF Core (compiled)" ]
-    |> Chart.Plot
-    |> Chart.WithLayout groupedLayout
-    |> Chart.WithSize (768, 512)
-    |> Chart.Show
+    |> Chart.combine
+    |> Chart.withYAxisStyle "Zeit [ms]"
+    |> Chart.withLayout groupedLayout
+    |> Chart.withSize (768., 512.)
+    |> Chart.show
     0
